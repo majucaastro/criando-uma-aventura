6ae140d4d4e50c904154f6f249e7d7414fdff15c
majucaastro <0000109906465xsp@al.educacao.sp.gov.br>
2024-08-30 14:59:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const avança = document.querySelectorAl('.btn-proximo');
+
+avança.forEach(button => {
+    button.addEventListener('click', function(){
+      const atual = document.querySelector('.ativo');
+      const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+      atual.classList.remove('ativo');
+      document.getElementById(proximoPasso).classList.add('ativo');
+    
+    })
+})
